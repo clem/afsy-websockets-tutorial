1da47a989352a5faed53581515809fb18ad390aa
Clément Delmas <clement.delmas@gmail.com>
2017-12-06 14:10:07 +0100

    Major release - End of AFSY tutorial
    - Update index.html.twig to add send message form
    - Update Chat.php to handle JSON messages and add some actions
    - Update sf-websocket.js to handle JSON messages and add user management

--- src/AppBundle/Resources/public/js/sf-websocket.js
+++ src/AppBundle/Resources/public/js/sf-websocket.js
@@ -2,34 +2,106 @@
 (function () {
   'use strict';
 
-  // Initialize
+  // Initialize Chat
+  var defaultChannel = 'general';
+  var botName = 'ChatBot';
+
+  // Initialize form
+  var _textInput = document.getElementById('ws-content-to-send');
+  var _textSender = document.getElementById('ws-send-content');
   var _receiver = document.getElementById('ws-content-receiver');
+  var enterKeyCode = 13;
+
+  // Ask user's name
+  var userName = prompt('Hi! I need your name for the Chat please :)');
+
+  /**
+   * Send _textInput content to WebSocket
+   */
+  var sendTextInputContent = function () {
+    // Get text input content
+    var content = _textInput.value;
+
+    // Send it to WS
+    ws.send(JSON.stringify({
+      action: 'message',
+      user: userName,
+      message: content,
+      channel: 'general'
+    }));
+
+    // Reset input
+    _textInput.value = '';
+  };
+
+  /**
+   * Add message to current channel
+   *
+   * @param {string} message - Message to display
+   */
+  var addMessageToChannel = function(message) {
+    _receiver.innerHTML += '<div class="message">' + message + '</div>';
+  };
+
+  /**
+   * Send a message from ChatBot to #general channel
+   *
+   * @param {string} message - Message to display
+   */
+  var botMessageToGeneral = function (message) {
+    return addMessageToChannel(JSON.stringify({
+      action: 'message',
+      channel: defaultChannel,
+      user: botName,
+      message: message
+    }));
+  };
+
+  // Initialize WebSocket
   var ws = new WebSocket('ws://' + wsUrl);
+  botMessageToGeneral('Connecting...');
 
   /**
    * On connection open
    */
   ws.onopen = function () {
-    ws.send('Hello');
-    _receiver.innerHTML = 'Connected !';
+    ws.send(JSON.stringify({
+      action: 'subscribe',
+      channel: defaultChannel,
+      user: userName
+    }));
   };
 
+  /**
+   * On message reception
+   *
+   * @param event
+   */
   ws.onmessage = function (event) {
-    _receiver.innerHTML = event.data;
+    addMessageToChannel(event.data);
   };
 
   /**
    * On connection close
    */
   ws.onclose = function () {
-    _receiver.innerHTML = 'Connection closed';
+    botMessageToGeneral('Connection closed');
   };
 
   /**
    * On connection error
    */
   ws.onerror = function () {
-    _receiver.innerHTML = 'An error occured!';
+    botMessageToGeneral('An error occured!');
+  };
+
+  // Bind form events
+  _textSender.onclick = sendTextInputContent;
+  _textInput.onkeyup = function(e) {
+    // Check for Enter key
+    if (e.keyCode === enterKeyCode) {
+      sendTextInputContent();
+    }
   };
 
 })();
--- src/AppBundle/Server/Chat.php
+++ src/AppBundle/Server/Chat.php
@@ -13,17 +13,33 @@ class Chat implements MessageComponentInterface
     /**
      * @var \SplObjectStorage
      */
-    protected $clients;
-    
+    private $clients;
+
+    /**
+     * @var array
+     */
+    private $users = [];
+
+    /**
+     * @var string
+     */
+    private $botName = 'ChatBot';
+
+    /**
+     * @var string
+     */
+    private $defaultChannel = 'general';
+
     /**
      * Chat constructor
      */
-    function __construct()
+    public function __construct()
     {
         // Initialize
         $this->clients = new \SplObjectStorage();
+        $this->users = [];
     }
-    
+
     /**
      * A new websocket connection
      *
@@ -33,9 +49,19 @@ class Chat implements MessageComponentInterface
     {
         // Initialize: Add connection to clients list
         $this->clients->attach($conn);
-        
+        $this->users[$conn->resourceId] = [
+            'connection' => $conn,
+            'user' => '',
+            'channels' => []
+        ];
+
         // Send hello message
-        $conn->send(sprintf('New connection: Hello #%d', $conn->resourceId));
+        $conn->send(json_encode([
+            'action'  => 'message',
+            'channel' => $this->defaultChannel,
+            'user'    => $this->botName,
+            'message' => sprintf('Connection established. Welcome #%d!', $conn->resourceId)
+        ]));
     }
 
     /**
@@ -45,6 +71,17 @@ class Chat implements MessageComponentInterface
      */
     public function onClose(ConnectionInterface $closedConnection)
     {
+        // Send Goodbye message
+        $this->sendMessageToChannel(
+            $closedConnection,
+            $this->defaultChannel,
+            $this->botName,
+            $this->users[$closedConnection->resourceId]['user'].'has disconnected'
+        );
+
+        // Remove connection from users
+        unset($this->users[$closedConnection->resourceId]);
+
         // Detach connection and send message (for log purposes)
         $this->clients->detach($closedConnection);
         echo sprintf('Connection #%d has disconnected\n', $closedConnection->resourceId);
@@ -58,34 +95,135 @@ class Chat implements MessageComponentInterface
      */
     public function onError(ConnectionInterface $conn, \Exception $e)
     {
-        $conn->send('An error has occurred: '.$e->getMessage());
+        $conn->send(json_encode([
+            'action'  => 'message',
+            'channel' => $this->defaultChannel,
+            'user'    => $this->botName,
+            'message' => 'An error has occurred: '.$e->getMessage()
+        ]));
         $conn->close();
     }
 
     /**
      * Handle message sending
      *
-     * @param ConnectionInterface $from
+     * @param ConnectionInterface $conn
      * @param string $message
+     *
+     * @return boolean - False if message is not a valid JSON or action is invalid
      */
-    public function onMessage(ConnectionInterface $from, $message)
+    public function onMessage(ConnectionInterface $conn, $message)
     {
         // Initialize
-        $totalClients = count($this->clients) - 1;
-        echo vsprintf(
-            'Connection #%1$d sending message "%2$s" to %3$d other connection%4$s'."\n", [
-            $from->resourceId,
-            $message,
-            $totalClients,
-            $totalClients === 1 ? '' : 's'
-        ]);
-        
-        // Loop to send message to all clients... except the sender
-        foreach ($this->clients as $client) {
-            if ($from !== $client) {
-                // The sender is not the receiver, send to each client connected
-                $client->send($message);
+        $messageData = json_decode($message);
+
+        // Check message data
+        if ($messageData === null) {
+            return false;
+        }
+
+        // Initialize message data
+        $action = $messageData->action ?? 'unknown';
+        $channel = $messageData->channel ?? $this->defaultChannel;
+        $user = $messageData->user ?? $this->botName;
+        $message = $messageData->message ?? '';
+
+        // Check action
+        switch ($action) {
+            case 'subscribe':
+                $this->subscribeToChannel($conn, $channel, $user);
+                return true;
+            case 'unsubscribe':
+                $this->unsubscribeFromChannel($conn, $channel, $user);
+                return true;
+            case 'message':
+                return $this->sendMessageToChannel($conn, $channel, $user, $message);
+            default:
+                echo sprintf('Action "%s" is not supported yet!', $action);
+                break;
+        }
+
+        // Return error
+        return false;
+    }
+
+    /**
+     * Subscribe connection to a given channel
+     *
+     * @param ConnectionInterface $conn - Active connection
+     * @param $channel - Channel to subscribe to
+     * @param $user - Username of subscribed user
+     */
+    private function subscribeToChannel(ConnectionInterface $conn, $channel, $user)
+    {
+        // Add channel to connection channels
+        $this->users[$conn->resourceId]['channels'][$channel] = $channel;
+
+        // Send joined message to channel
+        $this->sendMessageToChannel(
+            $conn,
+            $channel,
+            $this->botName,
+            $user.' joined #'.$channel
+        );
+    }
+
+    /**
+     * Unsubscribe connection to a given channel
+     *
+     * @param ConnectionInterface $conn - Active connection
+     * @param $channel - Channel to unsubscribe from
+     * @param $user - Username of unsubscribed user
+     */
+    private function unsubscribeFromChannel(ConnectionInterface $conn, $channel, $user)
+    {
+        // Check connection
+        if (array_key_exists($channel, $this->users[$conn->resourceId]['channels'])) {
+            // Delete connection
+            unset($this->users[$conn->resourceId]['channels']);
+        }
+
+        // Send left message to channel
+        $this->sendMessageToChannel(
+            $conn,
+            $channel,
+            $this->botName,
+            $user.' left #'.$channel
+        );
+    }
+
+    /**
+     * Send message to all connections of a given channel
+     *
+     * @param ConnectionInterface $conn - Active connection
+     * @param $channel - Channel to send message to
+     * @param $user - User's username
+     * @param $message - User's message
+     *
+     * @return boolean - False if channel doesn't exists
+     */
+    private function sendMessageToChannel(ConnectionInterface $conn, $channel, $user, $message)
+    {
+        // Check if connection is linked to channel
+        if (!isset($this->users[$conn->resourceId]['channels'][$channel])) {
+            // Don't send message
+            return false;
+        }
+
+        // Loop to send message to all users
+        foreach ($this->users as $connectionId => $userConnection) {
+            // Check if user has subscribe to channel
+            if (array_key_exists($channel, $userConnection['channels'])) {
+                $userConnection['connection']->send(json_encode([
+                    'action' => 'message',
+                    'channel' => $channel,
+                    'user' => $user,
+                    'message' => $message
+                ]));
             }
         }
+
+        // Return success
+        return true;
     }
 }
